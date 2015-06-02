83d833d48d230f32a4372b9915f1b135a24918cd
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-06-02 21:30:16 +0530

    Update Call.java

--- app/src/main/java/websank/corp/mahisan/payurecharge/Call.java
+++ app/src/main/java/websank/corp/mahisan/payurecharge/Call.java
@@ -3,7 +3,7 @@ package websank.corp.mahisan.payurecharge;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
